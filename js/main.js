d810ba22246e1663410df1a555745067c2d0ffd1
galbalazs87 <galbalazs87@gmail.com>
2017-07-19 10:59:19 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+//Listen for form submit
+document.getElementById('myForm').addEventListener('submit', saveBookmark);
+
+function saveBookmark(e){
+  console.log('It works!');
+
+  e.preventDefault();
+}
